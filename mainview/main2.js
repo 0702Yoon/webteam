14ac2f0a93e7033f729aade1f40e3a2a5642ea35
sehyun0329 <124059197+sehyun0329@users.noreply.github.com>
2023-05-27 05:31:41 +0900

    Add files via upload

--- mainview/main2.js
+++ mainview/main2.js
@@ -1,3 +1,40 @@
+// 로그인 성공 후 index2.html 페이지에서 실행할 함수
+function greetUser(nickname) {
+  document.getElementById("greeting").textContent = nickname + "님 안녕하세요";
+}
+
+// 카카오 로그아웃 버튼 클릭 시 처리하는 함수
+function kakaoLogout() {
+  Kakao.Auth.logout(function () {
+    // 로그아웃 성공 시 index.html로 이동
+    window.location.href = "index.html";
+  });
+}
+
+// 페이지 로드 시 호출될 함수
+function initialize() {
+  Kakao.init("5655e0aa2e406b874cfaff134add8648"); // 카카오 앱의 JavaScript 키를 사용하여 Kakao.init()을 호출
+
+  // 카카오 로그인한 사용자의 닉네임 받아오기
+  Kakao.API.request({
+    url: "/v2/user/me",
+    success: function (response) {
+      greetUser(response.properties.nickname);
+    },
+    fail: function (error) {
+      console.log("Failed to get user info: " + JSON.stringify(error));
+    },
+  });
+
+  // 카카오 로그아웃 버튼에 이벤트 리스너 등록
+  document
+    .getElementById("kakao-logout-btn")
+    .addEventListener("click", kakaoLogout);
+}
+
+// 페이지 로드 시 초기화 함수 호출
+window.onload = initialize;
+
 let horizontalUnderLine = document.getElementById(
   "horizontal-underline"
 ); /*menu쪽 밑줄*/
@@ -52,7 +89,6 @@ function flip() {
 
 // 목록 나오게
 var communityChild = document.getElementById("communityChild");
-
 var community = document.getElementById("community");
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -71,22 +107,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
-
-// 카카오 로그인 버튼을 생성하고 이벤트를 처리하는 함수
-function createKakaoLoginButton() {
-  Kakao.init("5655e0aa2e406b874cfaff134add8648"); // 카카오 앱의 JavaScript 키를 사용하여 Kakao.init()을 호출
-
-  Kakao.Auth.createLoginButton({
-    container: "#kakao-login-btn",
-    success: function (authObj) {
-      // 로그인 성공 시 index2.html로 이동
-      window.location.href = "index2.html";
-    },
-    fail: function (err) {
-      alert("Kakao login failed: " + JSON.stringify(err));
-    },
-  });
-}
-
-// 페이지 로드 시 카카오 로그인 버튼 생성 함수 호출
-window.addEventListener("DOMContentLoaded", createKakaoLoginButton);
